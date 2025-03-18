648d5684278a92f7261a82184b7da3172585a57b
Oleg <levenchuk.o@gmail.com>
2025-03-18 22:14:54 +0200

    dev server configuration

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,22 +4,29 @@ import { buildPlugins } from './config/build/buildPlugins';
 import { buildResolvers } from './config/build/buildResolvers';
 import { buildLoaders } from './config/build/buildLoaders';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildPaths } from './config/build/types/config';
+import { BuildEnv, BuildPaths } from './config/build/types/config';
 
-const paths: BuildPaths = {
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  build: path.resolve(__dirname, 'build'),
-  html: path.resolve(__dirname, 'public', 'index.html')
 
-}
 
-const mode = 'development';
-const isDev = mode === 'development';
+export default (env: BuildEnv) => {
 
-const config: webpack.Configuration = buildWebpackConfig({
-  mode: 'development',
-  paths,
-  isDev
-})
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html')
+  
+  }
+  
+  const mode = env.mode || 'development';
+  const isDev = mode === 'development';
+  const PORT = env.port || 3000;
+  
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode: mode,
+    paths,
+    isDev,
+    port: PORT,
+  })
 
-export default config;
+  return config;
+};
