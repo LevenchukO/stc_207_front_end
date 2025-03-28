e360e87ce5204091192a99fc4edf808e58f97cf7
LevenchukO <levenchuk.o@gmail.com>
2025-03-28 20:15:03 +0200

    added change theme logic based on contextProvider and hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import ThemeProvider from "./theme/ThemeProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );
