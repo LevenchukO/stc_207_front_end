e812a3daae80ee0bc1c0fe5c8302f4c2973c0b03
LevenchukO <levenchuk.o@gmail.com>
2025-04-04 10:51:40 +0300

    router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,9 @@
-import { Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
 import { Link } from "react-router-dom";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
-import { Suspense } from "react";
+
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/themeProvider";
+import { AppRouter } from "./providers/router";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -16,12 +14,7 @@ const App = () => {
       <Link to={"/"}>Main</Link>
       <Link to={"/about"}>About</Link>
 
-      <Suspense fallback={<div>Загрузка...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
