2dfb59dd4eeefd2636e3a970da8aac89807d94d4
LevenchukO <levenchuk.o@gmail.com>
2025-04-07 14:58:10 +0300

    added sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/themeProvider";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -12,8 +13,10 @@ const App = () => {
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
-
-      <AppRouter />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 };
